300b0081ef078afbe3c0ccfe0ebf46e1ad46c867
tramngyn <bvnguyen115@student.hbuhsd.edu>
2025-03-31 11:04:24 -0700

    Finished SpecialtyCoffee.java

---
+++ SpecialtyCoffee.java
@@ -0,0 +1,28 @@
+public class SpecialtyCoffee extends Coffee
+{
+    private String flavor;
+    
+    public SpecialtyCoffee() 
+    {
+        super();
+        flavor = "vanilla";
+    }
+
+    public SpecialtyCoffee(String size, String type, String flavor)
+    {
+        this.size = size;
+        this.type = type;
+        this.flavor = flavor;
+    }
+
+    public SpecialtyCoffee(String size, boolean isSkinny, int shots, String type, String flavor)
+    {
+        super(size, isSkinny, shots, type);
+        this.flavor = flavor;
+    }
+
+    public String toString()
+    {
+        return super.toString() + " with " + flavor;
+    }
+}
