b82a6c5cda2ae1093b1e66a4d9888947d7d367e8
tramngyn <bvnguyen115@student.hbuhsd.edu>
2025-04-01 12:09:58 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ public class Main
 {
   public static void main(String[] args)
   {
-    SpecialtyCoffee latte1 = new SpecialtyCoffee("large", "mocha", "caramel");
-    System.out.println(latte1.toString());
+    Coffee c = new Coffee("large", true, 10, "latte");
+    SpecialtyCoffee latte1 = new SpecialtyCoffee("large", false, 10, "mocha", "caramel");
+    System.out.println(c);
+    System.out.println(latte1);
   }
 }
